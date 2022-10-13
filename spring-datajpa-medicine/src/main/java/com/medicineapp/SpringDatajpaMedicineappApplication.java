7529b840b8b668516db8eb613e278da13c5e8857
MathanRajS <MathanRajS@Mathanraj.bbrouter>
2022-10-13 09:42:41 +0530

    Spring JPA Medicine App

--- spring-datajpa-medicine/src/main/java/com/medicineapp/SpringDatajpaMedicineappApplication.java
+++ spring-datajpa-medicine/src/main/java/com/medicineapp/SpringDatajpaMedicineappApplication.java
@@ -22,32 +22,34 @@ public class SpringDatajpaMedicineappApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		System.out.println("Add Medicine....");
-//		Medicine addMedicine=new Medicine("Rolax",221,"Food Poisoning","SKS company",200,true);
-//		medicineService.addMedicine(addMedicine);
-//		System.out.println("Update Medicine....");
-//		Medicine updateMedicine=new Medicine("roval",221,"covid","SKS company",500,true);
-//		medicineService.addMedicine(updateMedicine);
-//		System.out.println("Get By ID.....");
-//		Medicine medicineId=medicineService.getById(221);
-//		System.out.println(medicineId);
-//		System.out.println("Delete Medicine.....");
-//		medicineService.deleteMedicine(32);
+		System.out.println("Add Medicine....");
+		Medicine addMedicine=new Medicine("Rolax",221,"Food Poisoning","SKS company",200,true);
+		medicineService.addMedicine(addMedicine);
+		System.out.println("Update Medicine....");
+		Medicine updateMedicine=new Medicine("roval",221,"covid","SKS company",500,true);
+		medicineService.addMedicine(updateMedicine);
+		System.out.println("Get By ID.....");
+		Medicine medicineId=medicineService.getById(221);
+		System.out.println(medicineId);
+		System.out.println("Delete Medicine.....");
+		medicineService.deleteMedicine(32);
 		
 		
-//		System.out.println("Medicine Name Containing....");
-//		medicineService.getByNameContaining("r").forEach(System.out::println);
-//		System.out.println("Medicine Category....");
-//		medicineService.getByCategory("covid").forEach(System.out::println);
+		System.out.println("Medicine Name Containing....");
+		medicineService.getByNameContaining("r").forEach(System.out::println);
+		System.out.println("Medicine Category....");
+		medicineService.getByCategory("covid").forEach(System.out::println);
 		
-//		System.out.println("Medicine Name and Category....");
-//		medicineService.getByNameAndCategory("roval", "covid").forEach(System.out::println);
-//		System.out.println("Medicine Category And Brand....");
-//		medicineService.getByCategoryAndBrand("covid", "SKS company").forEach(System.out::println);
-//		System.out.println("Medicine Category And Cost....");
-//		medicineService.getByCategoryAndLessCost("covid", 500).forEach(System.out::println);
-//		System.out.println("Medicine Name and Availability....");
-//		medicineService.getByNameAndAvailabilty("roval", true).forEach(System.out::println);
+		System.out.println("Medicine Name and Category....");
+		medicineService.getByNameAndCategory("roval", "covid").forEach(System.out::println);
+		System.out.println("Medicine Category And Brand....");
+		medicineService.getByCategoryAndBrand("covid", "SKS company").forEach(System.out::println);
+		System.out.println("Medicine Category And Cost....");
+		medicineService.getByCategoryAndLessCost("covid", 500).forEach(System.out::println);
+		System.out.println("Medicine Name and Availability....");
+		medicineService.getByNameAndAvailabilty("roval", true).forEach(System.out::println);
+		
+		
 	}
 	
 
